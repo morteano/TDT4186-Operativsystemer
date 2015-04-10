107825a34af21c7207cc66dccc45560a3e4e47a4
Morten <morteano@stud.ntnu.no>
2015-04-10 21:17:03 +0200

    initial commit for CPU

---
+++ P3/CPU.java
@@ -0,0 +1,11 @@
+/**
+ * Created by MortenAlver on 08.04.2015.
+ */
+public class CPU {
+    /** The queue of processes waiting for CPU */
+    private Queue CPUQueue;
+    /** A reference to the statistics collector */
+    private Statistics statistics;
+    
+
+}
